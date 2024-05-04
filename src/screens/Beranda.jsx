cedda53f1feb893f62417d9708d848a0d68c3631
faisol35 <muhammadfaisolansori@gmail.com>
2024-05-04 15:37:53 +0700

    membuat data icon

--- src/screens/Beranda.jsx
+++ src/screens/Beranda.jsx
@@ -1,10 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import MenuIcon from '../component/MenuIcon';
+
+const Data = [
+  {id: 1, label: 'Top Up'},
+  {id: 2, label: 'Top Up'},
+  {id: 3, label: 'Top Up'},
+];
 
 const Beranda = () => {
   return (
     <View>
-      <Text>Beranda</Text>
+      <FlatList
+        data={Data}
+        renderItem={({item}) => <MenuIcon label={item.label} />}
+      />
     </View>
   );
 };
