422447542701af8ac294ead84ea1e819604974d2
faisol35 <muhammadfaisolansori@gmail.com>
2024-05-04 15:24:34 +0700

    membuat tampilan icon

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -1,11 +1,14 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 
 const MenuIcon = () => {
   return (
-    <View>
-      <Text>MenuIcon</Text>
-    </View>
+    <TouchableOpacity>
+      <View>
+        <Text>Icon</Text>
+      </View>
+      <Text>LABEL</Text>
+    </TouchableOpacity>
   );
 };
 
