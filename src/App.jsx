d4a2233b920c4003775fb5637bbd2d0093636e56
faisol35 <muhammadfaisolansori@gmail.com>
2024-05-04 15:57:34 +0700

    memberikan import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Beranda from './screens/Beranda';
+import Setting from './screens/Setting';
+import Profile from './screens/Profile';
 
 const App = () => {
   return (
