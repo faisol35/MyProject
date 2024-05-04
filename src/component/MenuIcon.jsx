8426c0e8b944286e8c30ae0d5d80d4b98404ed2d
faisol35 <muhammadfaisolansori@gmail.com>
2024-05-04 14:26:33 +0700

    pengeditan di menu icon

---
+++ src/component/MenuIcon.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const MenuIcon = () => {
+  return (
+    <View>
+      <Text>MenuIcon</Text>
+    </View>
+  );
+};
+
+export default MenuIcon;
+
+const styles = StyleSheet.create({});
